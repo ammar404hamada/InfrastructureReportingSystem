fc1b60296cd085e4c93bd41a4f1f311f79b562a7
Mostafa-AbuHamed <mostafa.ahmed.abuhamed@gmail.com>
2026-02-28 23:36:05 +0200

    Add AppDbContext, move app-settings to .git-ignor

--- InfraReportingSystem.Domain/Entities/ReportPic.cs
+++ InfraReportingSystem.Domain/Entities/ReportPic.cs
@@ -9,9 +9,10 @@ namespace InfraReportingSystem.Domain.Entities {
 
     public class ReportPic
     {
+        [Key]
         public int PicId { get; set; }
         [Required(ErrorMessage ="Picture URL is required!")]
-        public string PicUrl { get; set; }
+        public string PicUrl { get; set; } = string.Empty; 
         public int ReportId { get; set; }
         public Report Report { get; set; } = null!;
     }
--- InfrastructureReportingSystem/Program.cs
+++ InfrastructureReportingSystem/Program.cs
@@ -1,4 +1,7 @@
 
+using InfraReportingSystem.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace InfrastructureReportingSystem
 {
     public class Program
@@ -13,6 +16,8 @@ namespace InfrastructureReportingSystem
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
