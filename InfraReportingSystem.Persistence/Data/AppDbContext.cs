1146e06905f48a254d5c1154d26b5691f3ec88cb
ammar404hamada <ammar404hamada@gmail.com>
2026-03-03 00:25:40 +0200

    refactor(persistence): simplify DbContext and enable configuration assembly scanning

--- InfraReportingSystem.Persistence/Data/AppDbContext.cs
+++ InfraReportingSystem.Persistence/Data/AppDbContext.cs
@@ -6,27 +6,29 @@ using System.Text;
 using System.Threading.Tasks;
 using InfraReportingSystem.Domain.Entities;
 
-namespace InfraReportingSystem.Persistence.Data { 
-
+namespace InfraReportingSystem.Persistence.Data
+{
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        { 
-        
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+      
+        public DbSet<User> Users { get; set; }
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<Authority> Authorities { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
         public DbSet<Report> Reports { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<ReportPic> ReportPics { get; set; }
 
+        public DbSet<OtpVerification> OtpVerifications { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Report>()
-                .Property(r => r.Status)
-                .HasConversion<string>();
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
         }
     }
 }
