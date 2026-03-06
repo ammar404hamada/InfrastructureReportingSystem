0e33e4c8218ade5ac317c2968228879a793ffe3d
ammar404hamada <ammar404hamada@gmail.com>
2026-03-06 23:52:00 +0200

    feat(domain): add AuditLog entity and AuditActionType enum

---
+++ InfraReportingSystem.Domain/Entities/AuditLog.cs
@@ -0,0 +1,36 @@
+﻿using InfraReportingSystem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraReportingSystem.Domain.Entities
+{
+    public class AuditLog
+    {
+
+        public int Id { get; set; }
+
+        // The user who performed the action
+        // Nullable because failed login attempts may not belong to a valid user
+        public int? UserId { get; set; }
+        public User? User { get; set; }
+
+        // The type of action performed in the system
+        public AuditActionType ActionType { get; set; }
+
+        // The entity affected by the action (e.g., "Report", "User")
+        public string? EntityName { get; set; }
+
+        // The ID of the affected entity
+        public int? EntityId { get; set; }
+
+        // Detailed description of the action
+        public string Details { get; set; } = null!;
+
+        // Timestamp of when the action occurred
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+    }
+}
