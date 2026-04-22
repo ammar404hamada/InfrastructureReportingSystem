0f93157f29e48567b26ba49090ffc4730a864ab4
ammar404hamada <ammar404hamada@gmail.com>
2026-04-23 00:10:14 +0200

    feat: add MarkFixedDto and MarkBlockedDto for current task actions

---
+++ InfraReportingSystem.Shared/DTOs/Worker/CurrentTaskScreen/MarkBlockedDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraReportingSystem.Shared.DTOs.Worker.CurrentTaskScreen
+{
+    public class MarkBlockedDto
+    {
+        [Required]
+        [MinLength(3)]
+        public string Reason { get; set; } = null!;
+    }
+}
