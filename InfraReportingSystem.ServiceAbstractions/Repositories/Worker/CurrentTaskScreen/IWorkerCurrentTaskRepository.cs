27bba24fe2fe36fdd9f95492873dee05c7582f83
ammar404hamada <ammar404hamada@gmail.com>
2026-04-22 21:35:14 +0200

    feat: add IWorkerCurrentTaskRepository and implementation

---
+++ InfraReportingSystem.ServiceAbstractions/Repositories/Worker/CurrentTaskScreen/IWorkerCurrentTaskRepository.cs
@@ -0,0 +1,14 @@
+﻿using InfraReportingSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraReportingSystem.ServiceAbstractions.Repositories.Worker.CurrentTaskScreen
+{
+    public interface IWorkerCurrentTaskRepository
+    {
+        Task<Report?> GetCurrentTaskAsync(string workerId);
+    }
+}
