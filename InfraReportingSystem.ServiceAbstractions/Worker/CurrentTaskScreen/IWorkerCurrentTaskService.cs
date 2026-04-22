00b162125055e9ca8bad32529a57d52854e38de9
ammar404hamada <ammar404hamada@gmail.com>
2026-04-22 21:38:17 +0200

    feat: add IWorkerCurrentTaskService and implementation with PaginatedResult

---
+++ InfraReportingSystem.ServiceAbstractions/Worker/CurrentTaskScreen/IWorkerCurrentTaskService.cs
@@ -0,0 +1,15 @@
+﻿using InfraReportingSystem.Shared.DTOs.Common;
+using InfraReportingSystem.Shared.DTOs.Worker.CurrentTaskScreen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraReportingSystem.ServiceAbstractions.Worker.CurrentTaskScreen
+{
+    public interface IWorkerCurrentTaskService
+    {
+        Task<PaginatedResult<CurrentTaskDto>> GetCurrentTaskAsync(string workerId);
+    }
+}
