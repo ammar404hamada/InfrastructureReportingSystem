da3a6983cf41d6f846298eea3c52b50caa5d70dc
ammar404hamada <ammar404hamada@gmail.com>
2026-05-01 01:02:36 +0300

    fix(swagger): add JWT Bearer security definition for Swagger UI authorize button

--- InfrastructureReportingSystem/Program.cs
+++ InfrastructureReportingSystem/Program.cs
@@ -44,6 +44,30 @@ namespace InfrastructureReportingSystem
             builder.Services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Infra Reporting API", Version = "v1" });
+            
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header
+                });
+            
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
             });
 
             builder.Services.AddDbContext<AppDbContext>(options =>
