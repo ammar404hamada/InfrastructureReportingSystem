164a45f27aec87440cfe1838156a776d55404c89
Abdulrahman-Samir <abdo123sam465@gmail.com>
2026-05-02 16:44:00 +0300

    final version (يارب يشتغل يارب  ... ومصطفي ميشوفش العبط اللي عامله )

--- InfraReportingSystem.Persistence/Repositories/Authority/AssignWorker/AuthorityAssignWorkerRepository.cs
+++ InfraReportingSystem.Persistence/Repositories/Authority/AssignWorker/AuthorityAssignWorkerRepository.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using InfraReportingSystem.Domain.Entities;
 using InfraReportingSystem.Persistence.Data;
+using InfraReportingSystem.ServiceAbstractions.Repositories.Authority;
 using InfraReportingSystem.ServiceAbstractions.Repositories.Authority.AssignWorker;
+using Microsoft.EntityFrameworkCore;
 
 namespace InfraReportingSystem.Persistence.Repositories.Authority.AssignWorker
 {
@@ -21,13 +23,13 @@ namespace InfraReportingSystem.Persistence.Repositories.Authority.AssignWorker
         public async Task<Report?> GetByIdAsync(int reportId)
         {
             return await _context.Reports
-                .Include(r => r.SubmittedBy)
+                .Include(r => r.SubmittedBy)       
                 .FirstOrDefaultAsync(r => r.Id == reportId);
         }
 
         public async Task SaveChangesAsync()
         {
-            await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();     
         }
     }
 }
