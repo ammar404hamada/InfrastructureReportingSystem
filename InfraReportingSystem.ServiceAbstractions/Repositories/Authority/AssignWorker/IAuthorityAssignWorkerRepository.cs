a410832c616671d7b69236fa56119a0d01e3e07b
Abdulrahman-Samir <abdo123sam465@gmail.com>
2026-05-02 16:24:21 +0300

    feat: add IAuthorityAssignWorkerRepository contract

---
+++ InfraReportingSystem.ServiceAbstractions/Repositories/Authority/AssignWorker/IAuthorityAssignWorkerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfraReportingSystem.Domain.Entities;
+
+namespace InfraReportingSystem.ServiceAbstractions.Repositories.Authority.AssignWorker
+{
+    public interface IAuthorityAssignWorkerRepository
+    {
+        Task<Report?> GetByIdAsync(int reportId);
+        Task SaveChangesAsync();
+    }
+}
