ac6e44daa30d1809bef337b2ce0abf231480da98
ammar404hamada <ammar404hamada@gmail.com>
2026-05-03 18:20:04 +0300

    feat: add AuditLogs service and repository abstractions

---
+++ InfraReportingSystem.ServiceAbstractions/Admin/AuditLogsScreen/IAdminAuditLogsService.cs
@@ -0,0 +1,16 @@
+﻿using InfraReportingSystem.Shared.DTOs.Admin.AuditLogsScreen;
+using InfraReportingSystem.Shared.DTOs.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraReportingSystem.ServiceAbstractions.Admin.AuditLogsScreen
+{
+    public interface IAdminAuditLogsService
+    {
+        Task<PaginatedResult<AuditLogDto>> GetLogsAsync(AuditLogsFilterDto filter);
+    }
+
+}
---
+++ InfraReportingSystem.ServiceAbstractions/Repositories/Admin/AuditLogsScreen/IAdminAuditLogsRepository.cs
@@ -0,0 +1,23 @@
+﻿using InfraReportingSystem.Domain.Entities;
+using InfraReportingSystem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraReportingSystem.ServiceAbstractions.Repositories.Admin.AuditLogsScreen
+{
+    public interface IAdminAuditLogsRepository
+    {
+        Task<(List<AuditLog> Logs, int TotalCount, Dictionary<string, string> UserRoles)> GetAuditLogsAsync(
+            int pageNumber,
+            int pageSize,
+            string? search,
+            DateTime? startDate,
+            DateTime? endDate,
+            string? normalizedRole,
+            AuditActionType? actionType);
+    }
+
+}
