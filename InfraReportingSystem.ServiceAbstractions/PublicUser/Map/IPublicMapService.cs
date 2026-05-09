ef8af4b38157772abdf94289e43ce85ebd0081c4
Abdulrahman-Samir <abdo123sam465@gmail.com>
2026-05-09 14:46:54 +0300

    feat: add IPublicMapService contract

---
+++ InfraReportingSystem.ServiceAbstractions/PublicUser/Map/IPublicMapService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfraReportingSystem.Domain.Enums;
+using InfraReportingSystem.Shared.DTOs.PublicUser.Map;
+
+namespace InfraReportingSystem.ServiceAbstractions.PublicUser.Map
+{
+    public interface IPublicMapService
+    {
+        Task<IEnumerable<MapReportDto>> GetMapReportsAsync(
+            int? categoryId,
+            ReportStatus? status);
+    }
+}
