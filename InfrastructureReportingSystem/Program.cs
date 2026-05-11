34ae409ec0f1bd4b1d7821baf5aa3bfc8699891e
Mostafa-AbuHamed <mostafa.ahmed.abuhamed@gmail.com>
2026-05-11 13:42:12 +0300

    Hot fix | Enable CORSConfig

--- InfrastructureReportingSystem/Program.cs
+++ InfrastructureReportingSystem/Program.cs
@@ -153,6 +153,16 @@ namespace InfrastructureReportingSystem
             // Register DataSeeder
             builder.Services.AddScoped<DataSeeder>();
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowFrontend", policy =>
+                {
+                    policy.WithOrigins("http://localhost:4200")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
             var app = builder.Build();
 
             // Database Migration and Seeding Pipeline
@@ -175,7 +185,7 @@ namespace InfrastructureReportingSystem
             });
 
             app.UseHttpsRedirection();
-
+            app.UseCors("AllowFrontend");
             app.UseAuthentication();
             app.UseAuthorization();
 
