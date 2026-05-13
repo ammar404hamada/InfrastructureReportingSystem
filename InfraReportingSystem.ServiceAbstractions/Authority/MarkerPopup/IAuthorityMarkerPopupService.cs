68d9c1f3cc3526b9bde88835e44d8633d880cc29
Abdulrahman-Samir <abdo123sam465@gmail.com>
2026-05-14 02:40:19 +0300

    feat: add IAuthorityMarkerPopupService contract

---
+++ InfraReportingSystem.ServiceAbstractions/Authority/MarkerPopup/IAuthorityMarkerPopupService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfraReportingSystem.Shared.DTOs.Authority.MarkerPopup;
+
+namespace InfraReportingSystem.ServiceAbstractions.Authority.MarkerPopup
+{
+    public interface IAuthorityMarkerPopupService
+    {
+        Task<AuthorityMarkerPopupDto?> GetReportPopupAsync(int reportId);
+    }
+}
