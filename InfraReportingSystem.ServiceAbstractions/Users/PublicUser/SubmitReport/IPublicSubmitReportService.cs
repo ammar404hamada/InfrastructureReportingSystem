db0ccfc46b9ccdbc66d05b71777f22204d28e4b0
Abdulrahman-Samir <abdo123sam465@gmail.com>
2026-06-14 12:08:22 +0300

    feat: add SubmitReport service and repository interfaces to ServiceAbstractions layer

---
+++ InfraReportingSystem.ServiceAbstractions/Users/PublicUser/SubmitReport/IPublicSubmitReportService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfraReportingSystem.Shared.DTOs.Users.PublicUser.SubmitReport;
+
+namespace InfraReportingSystem.ServiceAbstractions.Users.PublicUser.SubmitReport
+{
+    public interface IPublicSubmitReportService
+    {
+        Task<CategorySuggestionDto> GenerateCategoryAsync(Stream imageStream, string fileName);
+        Task<DescriptionSuggestionDto> GenerateDescriptionAsync(Stream imageStream, string fileName);
+        Task<SubmitReportResponseDto> SubmitReportAsync(SubmitReportRequestDto request, Stream imageStream, string fileName, string userId);
+    }
+}
