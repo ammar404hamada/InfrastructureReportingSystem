bfca31a3ebc963fd78f83fc88dfff6bc22864dbe
Abdulrahman-Samir <abdo123sam465@gmail.com>
2026-06-14 11:51:46 +0300

    feat: add SubmitReport DTOs to Shared layer

---
+++ InfraReportingSystem.Shared/DTOs/Users/PublicUser/SubmitReport/CategorySuggestionDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraReportingSystem.Shared.DTOs.Users.PublicUser.SubmitReport
+{
+    public class CategorySuggestionDto
+    {
+        public string SuggestedCategory { get; set; } = string.Empty;
+    }
+}
---
+++ InfraReportingSystem.Shared/DTOs/Users/PublicUser/SubmitReport/SubmitReportResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraReportingSystem.Shared.DTOs.Users.PublicUser.SubmitReport
+{
+    public class SubmitReportResponseDto
+    {
+        public int ReportId { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+    }
+}
