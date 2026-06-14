2b9b95e7f2a29207b0e58fdb692a5440199c5032
Abdulrahman-Samir <abdo123sam465@gmail.com>
2026-06-14 12:14:33 +0300

    feat: add PublicSubmitReportRepository to Persistence layer

---
+++ InfraReportingSystem.Persistence/Repositories/Users/PublicUser/SubmitReport/PublicSubmitReportRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using InfraReportingSystem.Domain.Entities;
+using InfraReportingSystem.Persistence.Data;
+using InfraReportingSystem.ServiceAbstractions.Repositories.Users.PublicUser.SubmitReport;
+
+namespace InfraReportingSystem.Persistence.Repositories.Users.PublicUser.SubmitReport
+{
+    public class PublicSubmitReportRepository : IPublicSubmitReportRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PublicSubmitReportRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Category?> GetCategoryByIdAsync(int categoryId)
+        {
+            return await _context.Categories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == categoryId);
+        }
+
+        public async Task AddReportAsync(Report report)
+        {
+            await _context.Reports.AddAsync(report);
+        }
+
+        public async Task AddReportPicAsync(ReportPic reportPic)
+        {
+            await _context.ReportPics.AddAsync(reportPic);
+        }
+
+        public async Task AddAuditLogAsync(AuditLog auditLog)
+        {
+            await _context.AuditLog.AddAsync(auditLog);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
