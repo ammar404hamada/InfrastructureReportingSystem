6f2617ccbd2c1e64bb9307c61c0cb7b59f3eddb5
Abdulrahman-Samir <abdo123sam465@gmail.com>
2026-06-14 04:10:06 +0300

    feat: add DTOs for PublicUser SubmitReport screen

---
+++ InfraReportingSystem.Shared/DTOs/Users/PublicUser/SubmitReport/SubmitReportRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraReportingSystem.Shared.DTOs.Users.PublicUser.SubmitReport
+{
+    public class SubmitReportRequestDto
+    {
+        public IFormFile Image { get; set; } = null!;
+        public int CategoryId { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
