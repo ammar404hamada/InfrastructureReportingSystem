eeebc7f8f3d205d79d93debfc813961f54bdb16d
Abdulrahman-Samir <abdo123sam465@gmail.com>
2026-06-14 04:15:44 +0300

    feat: add IPublicSubmitReportRepository abstraction

---
+++ InfraReportingSystem.ServiceAbstractions/Repositories/Users/PublicUser/SubmitReport/IPublicSubmitReportRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfraReportingSystem.Domain.Entities;
+
+namespace InfraReportingSystem.ServiceAbstractions.Repositories.Users.PublicUser.SubmitReport
+{
+    public interface IPublicSubmitReportRepository
+    {
+        Task<Category?> GetCategoryByIdAsync(int categoryId);
+        Task AddReportAsync(Report report);
+        Task AddReportPicAsync(ReportPic reportPic);
+        Task AddAuditLogAsync(AuditLog auditLog);
+        Task SaveChangesAsync();
+    }
+}
