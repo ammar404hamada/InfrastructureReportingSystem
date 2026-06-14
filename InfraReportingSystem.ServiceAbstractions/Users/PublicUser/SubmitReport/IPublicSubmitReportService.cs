8388883f0d8e7a15adcf41c5bb7986fe6110d73d
Abdulrahman-Samir <abdo123sam465@gmail.com>
2026-06-14 04:14:19 +0300

    feat: add IPublicSubmitReportService abstraction

---
+++ InfraReportingSystem.ServiceAbstractions/Users/PublicUser/SubmitReport/IPublicSubmitReportService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfraReportingSystem.Shared.DTOs.Users.PublicUser.SubmitReport;
+
+namespace InfraReportingSystem.ServiceAbstractions.Users.PublicUser.SubmitReport
+{
+    public interface IPublicSubmitReportService
+    {
+        Task<CategorySuggestionDto> GenerateCategoryAsync(IFormFile image);
+        Task<DescriptionSuggestionDto> GenerateDescriptionAsync(IFormFile image);
+        Task<SubmitReportResponseDto> SubmitReportAsync(SubmitReportRequestDto request, string userId);
+    }
+}
