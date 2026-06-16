53f4843cf3440187952971a9d0d905b7cd3baa6c
Mostafa-AbuHamed <mostafa.ahmed.abuhamed@gmail.com>
2026-06-16 11:26:22 +0300

    fix: add our AI model

---
+++ InfraReportingSystem.Domain/AI/AnalyzeImageResult.cs
@@ -0,0 +1,8 @@
+namespace InfraReportingSystem.Domain.AI
+{
+    public class AnalyzeImageResult
+    {
+        public string Prediction { get; set; } = string.Empty;
+        public string ImageDescription { get; set; } = string.Empty;
+    }
+}
